2c74acfdabe2f8fdbf5673bd917f8d7d25cc9d52
Saqib Suleman <saqibs3291@gmail.com>
2021-02-14 18:53:04 +0500

    add css

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const container = document.querySelector("#container");
 
 
 for (let i = 1; i < 899; i++) {
+    const pokemon = document.createElement("div");
+    pokemon.classList.add("pokemon");
+    const label = document.createElement("span");
+    label.innerText = `#${i}`;
     const newImg = document.createElement("img");
     newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-    container.appendChild(newImg);
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
 }
